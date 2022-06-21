0f4584b3e06ab6bd16c68ab26ed1bab8f42d0c2b
Peterson Paulo <peterpaulodev@gmail.com>
2022-06-21 16:50:09 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createProvider } from './vue-apollo'
+import babelPolyfill from 'babel-polyfill'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
+  babelPolyfill,
   render: h => h(App)
 }).$mount('#app')
